3c63257c159c91f5ff730d5603db8d24928cdce5
prince <princekumarkeshri.22@gmail.com>
2021-10-27 20:08:13 +0530

    All set

--- app/src/main/java/com/example/revisiontopicapplication/ScoreFragment.kt
+++ app/src/main/java/com/example/revisiontopicapplication/ScoreFragment.kt
@@ -7,13 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.revisiontopicapplication.databinding.FragmentScoreBinding
 
 
 class ScoreFragment : Fragment() {
 
-
+    private lateinit var viewModel: ScoreViewModel
+    private lateinit var viewModelFactory: ScoreViewModelFactory
 private lateinit var binding:FragmentScoreBinding
 
 
@@ -27,8 +32,26 @@ private lateinit var binding:FragmentScoreBinding
 
         //Get the args using by navArgs property delegate
         val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
-        binding.scoreTv.text =scoreFragmentArgs.score.toString()
-//        binding.playAgainButton.set
+
+        viewModelFactory = ScoreViewModelFactory(scoreFragmentArgs.score)
+        viewModel = ViewModelProvider(this,viewModelFactory)
+            .get(ScoreViewModel::class.java)
+
+
+        // Add observer for score
+        viewModel.score.observe(this, Observer { newScore ->
+            binding.scoreTv.text = newScore.toString()
+        })
+
+
+
+        // Navigates back to title when button is pressed
+       /* viewModel.eventPlayAgain.observe(this, Observer { playAgain ->
+            if (playAgain) {
+                findNavController().navigate(ScoreFragmentDirections.actionRestart())
+                viewModel.onPlayAgainComplete()
+            }
+        })*/
         return binding.root
     }
 
--- app/src/main/java/com/example/revisiontopicapplication/ScoreViewModel.kt
+++ app/src/main/java/com/example/revisiontopicapplication/ScoreViewModel.kt
@@ -6,12 +6,24 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.navArgs
 
-class ScoreViewModel:ViewModel() {
+class ScoreViewModel(finalScore:Int):ViewModel() {
+    private val _eventPlayAgain = MutableLiveData<Boolean>()
+    val eventPlayAgain: LiveData<Boolean>
+        get() = _eventPlayAgain
+
     private val _score = MutableLiveData<Int>()
-    val score :LiveData<Int>
-    get() = _score
+    val score: LiveData<Int>
+        get() = _score
+
     init {
+        _score.value = finalScore
+    }
 
+    fun onPlayAgain() {
+        _eventPlayAgain.value = true
+    }
 
+    fun onPlayAgainComplete() {
+        _eventPlayAgain.value = false
     }
 }
